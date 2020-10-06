eb65f39874d14da04e2b478b676e1d5848d296a6
MatthewGarcia5 <71661372+MatthewGarcia5@users.noreply.github.com>
2020-10-06 11:07:49 -0400

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
+class Lesson1_3 {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
 
-class Question2 {
-    public static void main(String[] args) {
+    System.out.println("What is your name?");
+    String x = scan.nextLine();
+    System.out.println("Hi " + x + ", how old are you?");
+    int y = scan.nextInt();
 
-        /* Write your code here */
-
-    }
+    System.out.println(x + " is " + y + " years old."); 
+  
+  }
 }
