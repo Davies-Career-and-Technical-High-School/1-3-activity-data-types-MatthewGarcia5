31d2739e12d8efb901ad71ac4fac1be4eb483e3a
MatthewGarcia5 <71661372+MatthewGarcia5@users.noreply.github.com>
2020-10-06 11:05:22 -0400

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
+class Lesson1_3 {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
 
-class Question1 {
-    public static void main(String[] args) {
+    System.out.println("Input a double");
+    double x = scan.nextDouble();
+    System.out.println("Do It again");
+    double y = scan.nextDouble();
 
-        /* Write your code here */
-
-    }
+    System.out.println(y + "        " + x); 
+  
+  }
 }
