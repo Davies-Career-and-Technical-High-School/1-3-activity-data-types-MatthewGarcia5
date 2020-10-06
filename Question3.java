73fd08b322e534408e985f77e97689d29544a087
MatthewGarcia5 <71661372+MatthewGarcia5@users.noreply.github.com>
2020-10-06 11:12:29 -0400

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
+class Lesson1_3 {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
 
-class Question3 {
-    public static void main(String[] args) {
+    System.out.println("Java is a programming language, true or false?");
+    boolean x = scan.nextBoolean();
+    System.out.println("Only numbers can be stored by variables in Java, true or false?");
+    boolean y = scan.nextBoolean();
+    System.out.println("There are only 2 possible values which can be held by a boolean variable, true or false?");
+    boolean z = scan.nextBoolean();
 
-        /* Write your code here */
-
-    }
+    System.out.println("Question 1 - Your answer: " + x + ". Correct answer: true");  
+    System.out.println("Question 2 - Your answer: " + y + ". Correct answer: false");  
+    System.out.println("Question 1 - Your answer: " + z + ". Correct answer: true");  
+  
+  }
 }
